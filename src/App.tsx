cd2e6bfd8332c3da28c6663b911a6d704e65bd86
coiluck <wvy.wingle@gmail.com>
2026-05-04 19:23:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+import './css/default.css';
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+export default function App() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
